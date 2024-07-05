7cc5e9678d06b6b769adddfcd633b0cef388513f
pawelek <k.pawelek@vp.pl>
2024-07-06 00:32:38 +0200

    add header from tailwindUI

---
+++ app/components/Logo.tsx
@@ -0,0 +1,3 @@
+export const Logo = () => {
+  return <h1 className="text-xl font-semibold leading-6">Quill</h1>;
+};
---
+++ app/components/NavLinks.tsx
@@ -0,0 +1,11 @@
+export default function NavLinks({ children }: { children: React.ReactNode }) {
+  return (
+    <a
+      href="#"
+      className="text-sm font-semibold leading-6"
+      style={{ color: "var(--text)" }}
+    >
+      {children}
+    </a>
+  );
+}
