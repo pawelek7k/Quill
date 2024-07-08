c3b2b4bcef67ac7d38dd21452a53c05a2225b123
pawelek <k.pawelek@vp.pl>
2024-07-08 17:49:35 +0200

    add login container

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,11 @@
 import { Header } from "../components/Header";
+import { LoginContainer } from "../components/LoginContainer";
 
 export default function LogInPage() {
   return (
     <main>
       <Header />
-      <h1>login</h1>
+      <LoginContainer />
     </main>
   );
 }
