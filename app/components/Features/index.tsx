f3f7890f71ddc86d2090ee7e8c0489b5e0ab9544
pawelek <k.pawelek@vp.pl>
2024-07-10 15:02:37 +0200

    Update index.tsx

--- app/components/Features/index.tsx
+++ app/components/Features/index.tsx
@@ -1,6 +1,8 @@
 import { CardContainer } from "../Card";
+import { Paragraph } from "../Paragraph";
 import { Heading } from "../SecondaryHeading";
 import Section from "../Section";
+import { ThirdHeading } from "../ThirdHeading";
 
 const features = [
   {
@@ -41,54 +43,53 @@ const features = [
 export const Features = () => {
   return (
     <Section>
-      <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto lg:mx-0">
-            <Heading>Features</Heading>
-            <p className="mt-2 text-lg leading-8 text-gray-600 text-center">
-              Learn how to grow your business with our expert advice.
-            </p>
-          </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {features.map((post) => (
-              <article
-                key={post.id}
-                className="flex max-w-xl flex-col items-start justify-between"
-              >
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-gray-500">
-                    {post.date}
-                  </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {post.category.title}
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto lg:mx-0 text-center">
+          <Heading>Features</Heading>
+          <Paragraph>
+            {" "}
+            Learn how to grow your business with our expert advice.
+          </Paragraph>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {features.map((post) => (
+            <article
+              key={post.id}
+              className="flex max-w-xl flex-col items-start justify-between"
+            >
+              <div className="flex items-center gap-x-4 text-xs">
+                <time dateTime={post.datetime} className="text-gray-500">
+                  {post.date}
+                </time>
+                <a
+                  href={post.category.href}
+                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {post.category.title}
+                </a>
+              </div>
+              <div className="group relative">
+                <ThirdHeading>
+                  <a href={post.href}>
+                    <span className="absolute inset-0" />
+                    {post.title}
                   </a>
-                </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-                    {post.description}
-                  </p>
-                </div>
-                <ul className="relative mt-8 flex items-center gap-x-4">
-                  <CardContainer
-                    item={{
-                      name: post.title,
-                      src: post.imageUrl,
-                      alt: post.title,
-                    }}
-                  />
-                </ul>
-              </article>
-            ))}
-          </div>
+                </ThirdHeading>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                  {post.description}
+                </p>
+              </div>
+              <ul className="relative mt-8 flex items-center gap-x-4">
+                <CardContainer
+                  item={{
+                    name: post.title,
+                    src: post.imageUrl,
+                    alt: post.title,
+                  }}
+                />
+              </ul>
+            </article>
+          ))}
         </div>
       </div>
     </Section>
